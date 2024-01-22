05a6cf994b4437dfdf12d3860ea6a3b38bc6c9af
tomas f <46322567+TomFern@users.noreply.github.com>
2024-01-22 19:38:41 -0300

    Add GH comment to pipeline

--- src/gh_comment_ci.py
+++ src/gh_comment_ci.py
@@ -1,3 +1,6 @@
+# Usage:
+# gh_comment_cy.py path/to/report.md
+
 import requests
 from requests.auth import HTTPBasicAuth
 import os
@@ -8,30 +11,28 @@ token = os.getenv('GITHUB_ACCESS_TOKEN')
 repo_slug = os.getenv('SEMAPHORE_GIT_REPO_SLUG')
 commit_sha = os.getenv('SEMAPHORE_GIT_SHA')
 
-# Set your comment
-report_fn = sys.argv[1]
+if token is None:
+    print("GITHUB_ACCESS_TOKEN not set. Silently skipping commit comment.")
+    sys.exit(0)
+
+try:
+    report_fn = sys.argv[1]
+except:
+    print(f"Usage:\n  python {sys.argv[0]} path/to/report.md")
+    sys.exit(1)
+
 with open(report_fn, 'r') as f:
     comment = f.read()
 
-# comment = 'This is a comment on a commit.'
-
-# Construct the URL
 url = f'https://api.github.com/repos/{repo_slug}/commits/{commit_sha}/comments'
-print(url)
-print(comment)
-
-# Make a POST request to the GitHub API endpoint
-# response = requests.post(url, json={'body': comment}, auth=HTTPBasicAuth(username, token))
 headers = {
     'Authorization': f'Bearer {token}', 
     'X-GitHub-Api-Version': '2022-11-28' 
 }
-print(headers)
 response = requests.post(url, headers=headers, json={'body': comment})
 
 # Check the response
-if response.status_code == 201:
-    print('Comment posted successfully on GitHub.')
-else:
+if response.status_code != 201:
     print('Failed to post comment on GitHub.')
     print('Response:', response.content)
+    sys.exit(1)
